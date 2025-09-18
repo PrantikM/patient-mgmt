7c898b3ace1d8fa2fe8c8ba757d697b8abb15516
PrantikM <145676123+PrantikM@users.noreply.github.com>
2025-09-18 21:57:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,22 @@ def view_patient(patient_id: str = Path(..., description="The ID of the patient 
    if patient_id in data:
       return data[patient_id]
    raise HTTPException(status_code=404, detail="Patient not found")
+
+@app.get("/sort")
+def sort_patients(sort_by : str = Query(..., decription = "sort by height ,weight or bmi"),
+                  order :str = Query("asc", description="dort in asc or desc order")):
+
+   valid_fields = ["height", "weight", "bmi"]
+   if sort_by not in valid_fields:
+      raise HTTPException(status_code=400, detail=f"Invalid sort_by field. Must be one of {valid_fields}")
+
+   if order not in ["asc", "desc"]:
+      raise HTTPException(status_code=400, detail="Invalid order. Must be 'asc' or 'desc'")
+
+   data = load_data()
+
+   sort_order = True if order == "desc" else False
+
+   sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+   return sorted_data
